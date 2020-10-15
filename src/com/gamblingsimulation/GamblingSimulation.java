79288bcda85ed1e658ddb3c41f6e7f81a75c8f96
Avinash <sharma.avinash96@gmail.com>
2020-10-15 10:03:46 +0530

    UC-1

---
+++ src/com/gamblingsimulation/GamblingSimulation.java
@@ -0,0 +1,12 @@
+package com.gamblingsimulation;
+
+public class GamblingSimulation {
+        public static void main(String[] args) {
+            final int STAKE = 100;
+            final int BET=1;
+            for (int i=STAKE; i>0; i--){
+                System.out.println("stake: "+STAKE);
+
+            }
+        }
+}
