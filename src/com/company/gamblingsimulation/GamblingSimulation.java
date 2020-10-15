97117a9eb556db3b0169df32b207cc340aaadb65
Avinash <sharma.avinash96@gmail.com>
2020-10-15 12:50:52 +0530

    UC-3

--- src/com/company/gamblingsimulation/GamblingSimulation.java
+++ src/com/company/gamblingsimulation/GamblingSimulation.java
@@ -2,26 +2,35 @@ package com.company.gamblingsimulation;
 
 public class GamblingSimulation {
     static final int STAKE = 100;
-    static final int BET=1;
+    static final int BET = 1;
+    static final int win = 1;
 
     public static void main(String[] args) {
-        winLoose();
+        resignStake();
     }
 
     // UC-2 Win OR Loose
-    public static void winLoose(){
+    public static int winLoose() {
+        int random = (int) Math.round(Math.random());
+        if (random == win) {
+            return 1;
+        } else {
+            return 0;
+
+        }
+    }
+
+    public static void resignStake() {
         int cash = STAKE;
         final int target = 150;
         final int stop = 50;
         final int win = 1;
-        while(cash < target && cash > stop){
-            int random = (int)Math.round(Math.random());
-            if (random == win){
-                System.out.println("Win");
-                cash = cash+BET;
-            }else{
-                System.out.println("Loose");
-                cash = cash-BET;
+        while (cash < target && cash > stop) {
+            int random = winLoose();
+            if (random == win) {
+                cash = cash + BET;
+            } else {
+                cash = cash - BET;
             }
         }
         if (cash == 150){
