e766b6bce4502b3af38da38382c888ad9895d64b
Avinash <sharma.avinash96@gmail.com>
2020-10-15 10:41:21 +0530

    UC-2 WinLoose

--- src/com/gamblingsimulation/GamblingSimulation.java
+++ src/com/gamblingsimulation/GamblingSimulation.java
@@ -1,12 +1,18 @@
 package com.gamblingsimulation;
 
 public class GamblingSimulation {
-        public static void main(String[] args) {
-            final int STAKE = 100;
-            final int BET=1;
-            for (int i=STAKE; i>0; i--){
-                System.out.println("stake: "+STAKE);
-
-            }
+    public static void main(String[] args) {
+        final int STAKE = 100;
+        final int BET=1;
+        winLoose();
+    }
+    public static void winLoose(){
+        int win = 1;
+        int random = (int)Math.round(Math.random());
+        if (random == win){
+            System.out.println("Win");
+        }else{
+            System.out.println("Loose");
         }
+    }
 }
